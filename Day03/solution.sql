7db9a521ffedf1ed61d0721f0c1df87c7261dbf0
nsvp3205 <146202332+nsvp3205@users.noreply.github.com>
2026-02-17 21:35:13 +0530

    Update solution.sql

--- Day03/solution.sql
+++ Day03/solution.sql
@@ -1,4 +1,4 @@
-elect user_likes.user_id
+select user_likes.user_id
 from user_likes join video on user_likes.video_id = video.video_id
 where user_likes.reaction_type = 'LIKE'
 and cast(strftime("%Y", user_likes.reacted_at) as int) = 2021
